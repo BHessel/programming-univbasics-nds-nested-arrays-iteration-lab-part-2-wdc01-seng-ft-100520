6794982be15a6404b8592ddb6c9cba317241e37f
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-15 21:01:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,9 +6,10 @@ def find_min_in_nested_arrays(src)
     inside_pos
     while inside_pos < src[out_pos].count do
      if src[out_pos][inside_pos].min
-   end
-  end
+       min_count << src[out_pos][inside_pos]
+     end
+    end
   inside_pos += 1 
-end
+  end
 out_pos += 1 
 end
