c7d3388b9417b3352c49862b634c75c1e41a8e87
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-15 21:30:42 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,15 +4,20 @@ def find_min_in_nested_arrays(src)
   
   while array_row < src.count do
     array_element = 0 
-    min_element = n 
-    while array_element < src[array_row].count do              #while looping through the array_row/elements
-                            sdfsdfsdfsdfds                #if viewed element is the min in its array, push it to min_results
-       
+    while array_element < src[array_row].count do
+      src[array_row].min << min_results
+      
      end
-     inside_pos += 1 
     end
   min_count << min_number
   out_pos += 1 
   end
 min_count
 end
+
+
+#first I'm setting the start point for the array
+#then I'm giving it a new array (min_results) to push the minimum temperatures to
+#now I need to scan the first array, and set the position for counting the elements in that array
+#while looping through those elements, I need to isolate the min temperature and push it to the new array (min_results)
+#
